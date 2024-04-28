c9e8daffe14db1565c5dd28d3971a81b8eccb63e
xkattanr <xkattanr@fi.muni.cz>
2024-04-28 10:25:33 +0200

    feat(landing-page): basic layout

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import Navbar from '../components/navbar/Navbar';
 
 const Home = () => {
-  return <div>Landing Page</div>;
+  return (
+    <div className="flex flex-col h-screen gap-2 justify-between">
+      <div className="flex flex-row items-center gap-2 basis-2/6">
+        <div className="w-3/4 h-full bg-gray-300">Main Festival</div>
+        <div className="w-1/4 h-full bg-gray-300">News/Updates</div>
+      </div>
+      <div className="text-center bg-gray-300 basis-2/6">All Films</div>
+      <div className="text-center bg-gray-300 basis-1/6">Reservations</div>
+      <div className="text-center bg-gray-300 basis-1/6">Partners</div>
+    </div>
+  );
 };
 
 export default Home;
