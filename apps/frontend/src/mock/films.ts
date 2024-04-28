acc52facde82df714de1d056d8c012c981652e7d
xkattanr <xkattanr@fi.muni.cz>
2024-04-28 13:40:58 +0200

    feat(frontend): added mock data

---
+++ apps/frontend/src/mock/films.ts
@@ -0,0 +1,38 @@
+type MockFilm = {
+    title: string,
+    votes: number
+}
+
+export const films: MockFilm[] = [
+    { title: "Dune 2", votes: 69420 },
+    { title: "Winnie-the-Pooh: Blood and Honey", votes: -1 },
+    { title: "Inception", votes: 1500 },
+    { title: "The Shawshank Redemption", votes: 2000 },
+    { title: "The Godfather", votes: 1800 },
+    { title: "Pulp Fiction", votes: 1700 },
+    { title: "The Dark Knight", votes: 1900 },
+    { title: "Schindler's List", votes: 1600 },
+    { title: "Forrest Gump", votes: 2100 },
+    { title: "Fight Club", votes: 1400 },
+    { title: "The Lord of the Rings: The Return of the King", votes: 2200 },
+    { title: "The Matrix", votes: 1600 },
+    { title: "Goodfellas", votes: 1500 },
+    { title: "The Silence of the Lambs", votes: 1700 },
+    { title: "Saving Private Ryan", votes: 1900 },
+    { title: "The Green Mile", votes: 1800 },
+    { title: "The Usual Suspects", votes: 1600 },
+    { title: "Se7en", votes: 1700 },
+    { title: "Gladiator", votes: 2000 },
+    { title: "The Departed", votes: 1800 },
+    { title: "The Lion King", votes: 2100 },
+    { title: "Back to the Future", votes: 1900 },
+    { title: "The Prestige", votes: 1600 },
+    { title: "Inglourious Basterds", votes: 1700 },
+    { title: "The Godfather: Part II", votes: 1800 },
+    { title: "The Avengers", votes: 2000 },
+    { title: "The Shawshank Redemption", votes: 2100 },
+    { title: "The Lord of the Rings: The Fellowship of the Ring", votes: 2200 },
+    { title: "The Dark Knight Rises", votes: 1800 },
+    { title: "The Lord of the Rings: The Two Towers", votes: 2000 },
+    { title: "The Godfather: Part III", votes: 1700 }
+];
