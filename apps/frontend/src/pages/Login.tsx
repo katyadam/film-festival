e5b52e8f995f0c7ecd5043aa372849b29826979f
Adam Kattan <kattanadam7@gmail.com>
2024-05-17 16:56:00 +0200

    fix(landing-page): preventing merge issues

--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import LoginForm from '../components/forms/LoginForm';
 
 const Login = () => {
-  return (
-    <div className="flex align-center justify-around mt-12">
-      <LoginForm />
-    </div>
-  );
+  return <div>Login page</div>;
 };
 
 export default Login;
