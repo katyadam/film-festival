00f8edef40cc9d3a94654eb4dd07f092a5f73eed
Samuel Malec <xmalec@fi.muni.cz>
2024-06-14 18:50:23 +0200

    feat: implement film_api

--- apps/frontend/src/app/api/film_api.ts
+++ apps/frontend/src/app/api/film_api.ts
@@ -1,1 +1,32 @@
-const FILM_PREFIX = '/films';
+import BaseApi from './base_api';
+import { Film } from './types';
+
+const getAllFilms = async () => {
+  return await BaseApi.getAll<Film>('/films');
+};
+
+const getFilmById = async (id: number) => {
+  return await BaseApi.getSingle<Film>(`/films/${id}`);
+};
+
+const createFilm = async (film: Partial<Film>) => {
+  return await BaseApi.postSingle<Film>('/films', film);
+};
+
+const updateFilm = async (id: number, film: Partial<Film>) => {
+  return await BaseApi.putSingle<Film>(`/films/${id}`, film);
+};
+
+const deleteFilm = async (id: number) => {
+  return await BaseApi.deleteSingle<Film>(`/films/${id}`);
+};
+
+const FilmApi = {
+  getAllFilms,
+  getFilmById,
+  createFilm,
+  updateFilm,
+  deleteFilm,
+};
+
+export default FilmApi;
