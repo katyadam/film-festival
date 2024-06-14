c149a73e2cf412eec788efb1616da6a1cf29fcb1
xkattanr <xkattanr@fi.muni.cz>
2024-06-14 17:56:15 +0200

    feat(frontend): login page basic structure

--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import LoginForm from '../components/forms/LoginForm';
 
 const Login = () => {
-  return <div>Login page</div>;
+  return (
+    <div className="flex align-center justify-around mt-12">
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Login;
