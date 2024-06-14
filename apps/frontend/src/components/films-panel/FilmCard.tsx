a213c1d6612045a1f7235cdf5b5a3156995e2461
Adam Juhás <xjuhas1@fi.muni.cz>
2024-06-14 14:00:40 +0200

    basic structure

--- apps/frontend/src/components/films-panel/FilmCard.tsx
+++ apps/frontend/src/components/films-panel/FilmCard.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
 import { MockFilm } from '../../mock/films';
+import PlainButton from '../ui/PlainButton';
 
 type FilmCardProps = {
   film: MockFilm;
 };
 
 const FilmCard: FC<FilmCardProps> = ({ film }) => {
-  return <div></div>;
+  return <div className='bg-white p-6 rounded-lg shadow-md text-center'>
+    <h2 className='text-2xl mb-2'>{film.title}</h2>
+    <h4 className='mb-5'>Votes: {film.votes}</h4>
+    <PlainButton title='View more' link={film.title}></PlainButton>
+  </div>;
 };
 
 export default FilmCard;
--- apps/frontend/src/pages/Films.tsx
+++ apps/frontend/src/pages/Films.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import FilmFilter from '../components/films-panel/FilmFilter';
 import FilmRow from '../components/films-panel/FilmRow';
+import { films } from '../mock/films';
+
+
 
 const Films = () => {
   return (
-    <div className='min-h-screen p-8 mb-16'>
+    <div className='bg-gray-100 min-h-screen p-8 mb-16'>
       <div className='text-center mb-4'>
         <FilmFilter></FilmFilter>
       </div>
       <div>
-        <FilmRow></FilmRow>
+        <FilmRow films={films}></FilmRow>
       </div>
-
     </div>
   );
 };
