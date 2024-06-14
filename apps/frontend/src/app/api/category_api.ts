9663e4b3c0cfa58bbdd6f81a850e5afd3a7aeb08
Samuel Malec <xmalec@fi.muni.cz>
2024-06-14 18:50:14 +0200

    feat: implement category_api

--- apps/frontend/src/app/api/category_api.ts
+++ apps/frontend/src/app/api/category_api.ts
@@ -1,7 +1,32 @@
 import BaseApi from './base_api';
+import { Category } from './types';
 
-const CATEGORY_PREFIX = '/categories';
+const getAllCategories = async () => {
+  return await BaseApi.getAll<Category>('/categories');
+};
 
-async function getAll() {
-  return BaseApi.getAll(CATEGORY_PREFIX);
-}
+const getCategoryById = async (id: number) => {
+  return await BaseApi.getSingle<Category>(`/categories/${id}`);
+};
+
+const createCategory = async (category: Partial<Category>) => {
+  return await BaseApi.postSingle<Category>('/categories', category);
+};
+
+const updateCategory = async (id: number, category: Partial<Category>) => {
+  return await BaseApi.putSingle<Category>(`/categories/${id}`, category);
+};
+
+const deleteCategory = async (id: number) => {
+  return await BaseApi.deleteSingle<Category>(`/categories/${id}`);
+};
+
+const CategoryApi = {
+  getAllCategories,
+  getCategoryById,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+};
+
+export default CategoryApi;
