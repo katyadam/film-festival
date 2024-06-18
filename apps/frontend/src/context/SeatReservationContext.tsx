098c72f7420ff4c56b6a67bee974553b6c742e34
xkattanr <xkattanr@fi.muni.cz>
2024-06-18 10:34:34 +0200

    feat(reservation-logic): added context provider for seat reservation

---
+++ apps/frontend/src/context/SeatReservationContext.tsx
@@ -0,0 +1,67 @@
+import React, {
+  FC,
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useReducer,
+} from 'react';
+
+type Seat = { id: number; row: number; col: number };
+
+type SeatReservationState = {
+  seats: Seat[];
+};
+
+type SeatReservationAction =
+  | { type: 'BOOK_SEAT'; payload: Seat }
+  | { type: 'UNBOOK_SEAT'; payload: number };
+
+const SeatReservationContext = createContext<
+  | {
+      seatReservationState: SeatReservationState;
+      seatReservationDispatch: React.Dispatch<SeatReservationAction>;
+    }
+  | undefined
+>(undefined);
+
+const seatReservationReducer = (
+  state: SeatReservationState,
+  action: SeatReservationAction
+): SeatReservationState => {
+  switch (action.type) {
+    case 'BOOK_SEAT':
+      return { seats: [...state.seats, action.payload] };
+    case 'UNBOOK_SEAT':
+      return {
+        seats: state.seats.filter((seat) => seat.id !== action.payload),
+      };
+  }
+  return state;
+};
+
+export const SeatReservationProvider: FC<PropsWithChildren> = ({
+  children,
+}) => {
+  const [seatReservationState, seatReservationDispatch] = useReducer(
+    seatReservationReducer,
+    { seats: [] }
+  );
+
+  return (
+    <SeatReservationContext.Provider
+      value={{ seatReservationState, seatReservationDispatch }}
+    >
+      {children}
+    </SeatReservationContext.Provider>
+  );
+};
+
+export const useSeatReservation = () => {
+  const context = useContext(SeatReservationContext);
+  if (!context) {
+    throw new Error(
+      'useSeatReservation must be used within SeatReservationProvider!'
+    );
+  }
+  return context;
+};
