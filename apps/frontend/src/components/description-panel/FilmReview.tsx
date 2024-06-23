5af63944324740a59085c806a25bdc033d131886
xkattanr <xkattanr@fi.muni.cz>
2024-06-23 13:21:52 +0200

    fix(main): text overlap problem

--- apps/frontend/src/components/description-panel/FilmReview.tsx
+++ apps/frontend/src/components/description-panel/FilmReview.tsx
@@ -12,9 +12,11 @@ const FilmReview: FC<FilmReviewProps> = ({ review }) => {
   return (
     <div className="text-rose-900 font-semibold mb-4 bg-white p-6 rounded-lg shadow-md relative">
       {isSuccess && (
-        <p className="text-black text-3xl mb-2">{user.item.email}</p>
+        <p className="text-black text-3xl mb-2 break-words">
+          {user.item.email}
+        </p>
       )}
-      <p>{review.description}</p>
+      <p className="break-words">{review.description}</p>
     </div>
   );
 };
