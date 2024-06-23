c3e24f1853aeb703829bdea95e2931b434dcb19a
xkattanr <xkattanr@fi.muni.cz>
2024-06-23 07:58:50 +0200

    ref(main): seat button on reserved styling

--- apps/frontend/src/components/seat-reservation-table/SeatCell.tsx
+++ apps/frontend/src/components/seat-reservation-table/SeatCell.tsx
@@ -23,31 +23,36 @@ const SeatCell: FC<SeatCellProps> = ({ seat }) => {
     }
     setSelected(!selected);
   };
+  console.log(seat.reservationID);
 
-  return (
-    <button
-      className={`relative flex flex-col items-center justify-center border-2 h-8 w-10 lg:h-14 lg:w-20 cursor-pointer rounded-md ${
-        selected
-          ? 'bg-white'
-          : 'bg-rose-900 text-white border-white transform transition-all duration-300 hover:scale-105 hover:cursor-pointer hover:bg-white hover:text-rose-900'
-      } ${
-        seat.reservationID != null
-          ? 'bg-blue-500 hover:bg-blue-500 hover:text-white hover:scale-100 hover:cursor-default'
-          : ''
-      }`}
-      onClick={handleClick}
-      disabled={seat.reservationID != null}
-    >
-      {selected && (
-        <FontAwesomeIcon
-          className="absolute top-1 right-1 w-3 h-3 lg:w-5 lg:h-5"
-          icon={faRemove}
-          size="xl"
-        />
-      )}
-      <p>{id}</p>
-    </button>
-  );
+  if (seat.reservationID) {
+    return (
+      <div className="bg-black text-white relative flex flex-col items-center justify-center h-8 w-10 lg:h-14 lg:w-20 rounded-md">
+        Taken
+      </div>
+    );
+  } else {
+    return (
+      <button
+        className={`relative flex flex-col items-center justify-center border-2 h-8 w-10 lg:h-14 lg:w-20 cursor-pointer rounded-md ${
+          selected
+            ? 'bg-white'
+            : 'bg-rose-900 text-white border-white transform transition-all duration-300 hover:scale-105 hover:cursor-pointer hover:bg-white hover:text-rose-900'
+        } `}
+        onClick={handleClick}
+        disabled={seat.reservationID != null}
+      >
+        {selected && (
+          <FontAwesomeIcon
+            className="absolute top-1 right-1 w-3 h-3 lg:w-5 lg:h-5"
+            icon={faRemove}
+            size="xl"
+          />
+        )}
+        <p>{id}</p>
+      </button>
+    );
+  }
 };
 
 export default SeatCell;
