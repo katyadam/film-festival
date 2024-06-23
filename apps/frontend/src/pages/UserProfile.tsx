16def48fb126a8dd58e0fd2439ffb1a182909664
xkattanr <xkattanr@fi.muni.cz>
2024-06-23 11:07:53 +0200

    feat(main): better user profile responsivness

--- apps/frontend/src/pages/UserProfile.tsx
+++ apps/frontend/src/pages/UserProfile.tsx
@@ -10,14 +10,14 @@ const UserProfile = () => {
     <div className="bg-black min-h-screen text-white">
       <NavbarLine />
 
-      <div className="flex flex-row justify-between">
-        <div className="w-1/2 text-center border-2 rounded-md p-4 ml-2">
+      <div className="flex flex-col md:flex-row justify-between">
+        <div className="md:w-1/2 text-center border-2 rounded-md p-4 ml-2">
           <h1 className="text-4xl m-4">My reserved seats</h1>
           <NavbarLine />
           <ReservedSeatsTable />
         </div>
         {user && (
-          <div className="w-1/2 text-right mr-5">
+          <div className="md:w-1/2 text-right mr-5">
             <p>Email: {user?.email}</p>
             <p>Name: {user?.name}</p>
           </div>
